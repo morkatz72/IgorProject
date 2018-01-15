8512ad7b72e0c5c4e2696496f4fb93ca17d411ac
liormiz <liorstrongxd>
2018-01-15 22:03:07 +0200

    sss

--- Server/MainServer.js
+++ Server/MainServer.js
@@ -17,7 +17,7 @@ var app = express();
 
 // config app to use post api
 app.use( bodyParser.json());
-// app.use( bodyParser.options());
+// app.use( bodyParser.options());aa
 app.use(express.static(__dirname + "/src"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
