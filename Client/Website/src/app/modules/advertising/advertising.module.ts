702e485765f3cddbaabab778d1242ca505008984
morkatz72 <morka72@walla.com>
2018-01-21 16:39:59 +0200

    שינויים

--- Client/Website/src/app/modules/advertising/advertising.module.ts
+++ Client/Website/src/app/modules/advertising/advertising.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdvertisingService} from './advertising.service'
+import { AdvertisingService} from './advertising.service';
+import { AdvertisementComponent } from './advertisement/advertisement.component'
 
 @NgModule({
   imports: [
     CommonModule
   ],
   providers: [AdvertisingService],
-  declarations: []
+  declarations: [AdvertisementComponent]
 })
 export class AdvertisingModule { }
