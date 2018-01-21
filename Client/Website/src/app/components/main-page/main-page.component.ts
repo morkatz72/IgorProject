814a045b9f7e5b2a32826caf9d0ba4f14e86d0b3
liorstrongxd <liorstrongxd@BMW23>
2018-01-21 16:21:52 +0200

    2222

--- Client/Website/src/app/components/main-page/main-page.component.ts
+++ Client/Website/src/app/components/main-page/main-page.component.ts
@@ -20,6 +20,7 @@ export class MainPageComponent implements OnInit {
   w3_close() {
     document.getElementById("mySidebar").style.display = "none";
   }
+  //aaa
 
   addProductView() {
     this.router.navigate(['/add-or-update-product']);
