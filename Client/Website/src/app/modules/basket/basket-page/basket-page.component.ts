95cac492c05158518c580399bde6c81e5d980d93
Or Forte <or.forte1@gmail.com>
2018-02-07 19:57:43 +0200

    more changes

--- Client/Website/src/app/modules/basket/basket-page/basket-page.component.ts
+++ Client/Website/src/app/modules/basket/basket-page/basket-page.component.ts
@@ -3,6 +3,7 @@ import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BasketItemModule } from '../basket-item.module';
 import { CurrencyPipe } from '@angular/common';
+import { BasketService } from '../../../services/basketService/basket-service.service';
 
 @Component({
     selector: 'app-basket-page',
@@ -10,7 +11,7 @@ import { CurrencyPipe } from '@angular/common';
     styleUrls: ['./basket-page.component.css']
 })
 export class BasketPageComponent implements OnInit {
-    basketItems: BasketItemModule[];
+    basketItems: BasketItemModule[] = this.basketService.getBasket();
 
     getTotalPrice() {
         var totalPrice: number = 0;
@@ -21,36 +22,38 @@ export class BasketPageComponent implements OnInit {
     }
 
     removeItem(index: number) {
-        this.basketItems.splice(index, 1);
-        localStorage.setItem("basket", JSON.stringify(this.basketItems));
+        this.basketService.removeItemIndex(index);
+        this.basketItems = this.basketService.getBasket()
     }
 
     emptyBasket() {
-        this.basketItems = [];
-        localStorage.setItem("basket", JSON.stringify(this.basketItems));
+        this.basketService.setBasket([]);
+        this.basketItems = this.basketService.getBasket()
     }
 
-    constructor() { }
+    setItemAmount(productId: number, amount: number) {
+        this.basketService.setItemAmount(productId, amount)
+    }
+
+    constructor(private basketService : BasketService) { }
 
     ngOnInit() {
-        this.basketItems = JSON.parse(localStorage.getItem("basket"));
+        //this.basketItems = JSON.parse(localStorage.getItem("basket"));
 
-        if (this.basketItems.length == 0) {
-            this.basketItems.push(new BasketItemModule(
-                4,
-                "במבה אסם 50 גרם",
-                "https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/Milky_shokolad_446x302.jpg",
-                5.20,
-                3));
-            this.basketItems.push(new BasketItemModule(
-                5,
-                "מעדן חלב מילקי 125 מ\"ל",
-                "https://www.osem.co.il/tm-content/uploads/2015/01/Bamba_classic_80g3.png3-308x308.png",
-                3.70,
-                2));
-
-            localStorage.setItem("basket", JSON.stringify(this.basketItems));
-        }
+        //if (this.basketItems.length == 0) {
+        //    this.basketItems.push(new BasketItemModule(
+        //        4,
+        //        "במבה אסם 50 גרם",
+        //        "https://www.strauss-group.co.il/wp-content/blogs.dir/1/files/Milky_shokolad_446x302.jpg",
+        //        5.20,
+        //        3));
+        //    this.basketItems.push(new BasketItemModule(
+        //        5,
+        //        "מעדן חלב מילקי 125 מ\"ל",
+        //        "https://www.osem.co.il/tm-content/uploads/2015/01/Bamba_classic_80g3.png3-308x308.png",
+        //        3.70,
+        //        2));
+        //}
     }
 
 }
