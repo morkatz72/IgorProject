e2e52af8659369c33446a6f3dc67ac919dd80be5
liormiz <liorstrongxd>
2018-02-10 20:49:33 +0200

    סתם

--- Client/Website/src/app/app.component.ts
+++ Client/Website/src/app/app.component.ts
@@ -7,15 +7,19 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-
+  Snap: any;
   @ViewChild('myCanvas') myCanvas: ElementRef;
   public context: CanvasRenderingContext2D;
 
   ngOnInit() {
     localStorage.setItem("basket", "[]");
+    //this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
+
+    var canvas = this.Snap("#svg");
+    canvas.circle(5, 5, 5, 5).attr({ fill: "black" });
+    console.log('clicked');
+    console.log(this.title);
+    console.log(this.title);
+    canvas.circle(5, 5, 5, 5).attr({ fill: "red" });
   }
-  /*
-  ngAfterViewInit(): void {
-    this.context = (<HTMLCanvasElement>this.myCanvas.nativeElement).getContext('2d');
-  }*/
 }
