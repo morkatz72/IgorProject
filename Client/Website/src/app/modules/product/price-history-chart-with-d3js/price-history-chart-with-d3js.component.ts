7811d4bb8f485593c6e9b351a24e5521bfadef3d
liormiz <liorstrongxd>
2018-02-16 02:27:09 +0200

    הוספת גרף חדש

--- Client/Website/src/app/modules/product/price-history-chart-with-d3js/price-history-chart-with-d3js.component.ts
+++ Client/Website/src/app/modules/product/price-history-chart-with-d3js/price-history-chart-with-d3js.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-price-history-chart-with-d3js',
   templateUrl: './price-history-chart-with-d3js.component.html',
@@ -7,7 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceHistoryChartWithD3jsComponent implements OnInit {
 
-  constructor() { }
+  loadAPI: Promise<any>;
+
+
+  constructor() {
+    this.loadAPI = new Promise((resolve) => {
+      this.loadScript();
+      resolve(true);
+    });
+  }
+
+  public loadScript() {
+    var isFound = false;
+    var scripts = document.getElementsByTagName("script")
+    for (var i = 0; i < scripts.length; ++i) {
+      if (scripts[i].getAttribute('src') != null && scripts[i].getAttribute('src').includes("loader")) {
+        isFound = true;
+      }
+    }
+
+    if (!isFound) {
+      var dynamicScripts = ["chart.js"];
+
+      for (var i = 0; i < dynamicScripts.length; i++) {
+        let node = document.createElement('script');
+        node.src = dynamicScripts[i];
+        node.type = 'text/javascript';
+        node.async = false;
+        node.charset = 'utf-8';
+        document.getElementsByTagName('head')[0].appendChild(node);
+      }
+
+    }
+  }
 
   ngOnInit() {
   }
