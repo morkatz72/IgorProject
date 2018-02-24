4009c64034702b666202e97b56b28f2a29e24c95
liorstrongxd <liorstrongxd@gmail.com>
2018-02-24 15:48:10 +0200

    שינויים

--- Client/Website/src/app/modules/product/preferred-product/preferred-product.component.ts
+++ Client/Website/src/app/modules/product/preferred-product/preferred-product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../../../shared/entities/Category';
+import { ProductService } from '../product.service';
+import { EventEmitter } from '@angular/core';
+import { Product } from '../../../shared/entities/Product';
 
 @Component({
   selector: 'app-preferred-product',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreferredProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
+
+  public categories: Category[];
+  public category: number;
+  select: EventEmitter<string>;
+  public productIdToShow: any;
+  public CategoryValue: any;
+  public productDetails: Product;
+  public boolIsShow: boolean = false;
 
   ngOnInit() {
+    this.select = new EventEmitter();
+    this.productIdToShow = 0;
+    this.getCategories();
   }
 
+  getCategories() {
+    this.productService.getCategories().subscribe((results) => {
+      this.categories = Category.toCategories(results);
+      console.log(this.categories);
+    })
+  }
+
+  selectItem(value) {
+    this.select.emit(value);
+    this.category = +value;
+  }
+
+  chooseTheCheapestProduct() {
+    this.productService.getCheapestProductByCategory(this.category).subscribe(
+      (data) => {
+        console.log(data);
+        if (typeof data[0] !== 'undefined' && data[0] !== null) {
+          console.log(data[0]._productId);
+          this.productIdToShow = +data[0]._productId;
+          this.boolIsShow = true;
+
+        }
+        else {
+          this.productDetails = new Product();
+          this.productIdToShow = {};
+          this.boolIsShow = false;
+        }
+      }
+    );
+  }
+
+  getCategoryById(categoryId: number): any {
+    this.productService.getCategory(categoryId).subscribe(
+      (data) => {
+        this.CategoryValue = data[0];
+        this.productDetails.categoryValue = this.CategoryValue.name;
+        console.log(data);
+      }
+    );
+
+    return this.productDetails;
+  }
 }
