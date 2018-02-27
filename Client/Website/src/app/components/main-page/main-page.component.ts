54c016d31fa7b2a2fc161f706bfc6a198489f64d
liorstrongxd <liorstrongxd@gmail.com>
2018-02-27 21:37:20 +0200

    שינוי בעיצוב

--- Client/Website/src/app/components/main-page/main-page.component.ts
+++ Client/Website/src/app/components/main-page/main-page.component.ts
@@ -16,6 +16,14 @@ export class MainPageComponent implements OnInit {
   ngOnInit() {
   }
 
+  openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
   w3_open() {
     document.getElementById("mySidebar").style.display = "block";
   }
@@ -23,20 +31,25 @@ export class MainPageComponent implements OnInit {
     document.getElementById("mySidebar").style.display = "none";
   }
   addProductView() {
+    this.closeNav();
     this.router.navigate(['/add-or-update-product']);
   }
   loginView() {
+    this.closeNav();
     this.router.navigate(['/login'])
   }
   registerView() {
+    this.closeNav();
     this.router.navigate(['/register'])
   }
 
   productListView() {
+    this.closeNav();
     this.router.navigate(['/product-list'])
   }
 
   basketView() {
+    this.closeNav();
     this.router.navigate(['/basket'])
   }
 
@@ -49,31 +62,38 @@ export class MainPageComponent implements OnInit {
   }
 
   managerView() {
+    this.closeNav();
     this.router.navigate(['/manager-page'])
   }
 
   logOff() {
+    this.closeNav();
     localStorage.removeItem('currentUser');
     this.router.navigate(['/'])
   }
 
   HistoryOneView() {
+    this.closeNav();
     this.router.navigate(['/history-one-d3js/' + this.productId])
   }
 
   barChart() {
+    this.closeNav();
     this.router.navigate(['/history-two-chart-bar'])
   }
 
   setMainPage() {
+    this.closeNav();
     this.router.navigate(['/'])
   }
 
   cheapestProduct() {
+    this.closeNav();
     this.router.navigate(['/cheapest-product'])
   }
 
   preferredProduct() {
+    this.closeNav();
     this.router.navigate(['/preferred-product'])
   }
 }
